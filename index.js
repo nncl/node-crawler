5e8cadefcfb9ecb9e00088139ddd9ac23a45f029
= <nncl@live.com>
2019-02-03 20:55:49 -0200

    Adding crawler lib and testing simple data extraction

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const Crawler = require("crawler")
+    , c = new Crawler({
+    maxConnections: 10,
+    // This will be called for each crawled page
+    callback: (error, res, done) => {
+        if (error) {
+            console.log(error);
+        } else {
+            const $ = res.$;
+            // $ is Cheerio by default
+            //a lean implementation of core jQuery designed specifically for the server
+            console.log($("title").text());
+        }
+        done();
+    }
+});
+
+// Queues
+
+c.queue(['http://www.google.com/', 'http://www.yahoo.com']);
